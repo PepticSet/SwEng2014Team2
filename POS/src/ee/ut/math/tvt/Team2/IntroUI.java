1c6a38e469e2b3b298a0834c6c3aedd7409f5ba4
Martin Valgur <martin.valgur@gmail.com>
2014-10-14 23:36:32 +0300

    Fixed a typo.

--- POS/src/ee/ut/math/tvt/Team2/IntroUI.java
+++ POS/src/ee/ut/math/tvt/Team2/IntroUI.java
@@ -1,25 +1,23 @@
 package ee.ut.math.tvt.Team2;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
-
-import javax.swing.JLabel;
-
 import org.apache.log4j.Logger;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class IntroUI extends JFrame {
 	private static final long serialVersionUID = 1L;
-	
-	private static final Logger log = Logger.getLogger(IntroUI_.class);
+
+	private static final Logger log = Logger.getLogger(IntroUI.class);
 
 	private JPanel contentPane;
 
@@ -49,58 +47,57 @@ public class IntroUI extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
-		
+		contentPane.setLayout(new FormLayout(
+				new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC,
+						FormFactory.DEFAULT_COLSPEC,
+						FormFactory.RELATED_GAP_COLSPEC,
+						FormFactory.DEFAULT_COLSPEC, }, new RowSpec[] {
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC, }));
+
 		JLabel lblTeamName = new JLabel("Team name:");
 		contentPane.add(lblTeamName, "2, 2");
-		
+
 		JLabel teamNameDetails = new JLabel("");
 		contentPane.add(teamNameDetails, "4, 2");
-		
+
 		JLabel lblTeamLeader = new JLabel("Team leader:");
 		contentPane.add(lblTeamLeader, "2, 4");
-		
+
 		JLabel teamLeaderDetails = new JLabel("");
 		contentPane.add(teamLeaderDetails, "4, 4");
-		
+
 		JLabel lblTeamLeaderEmail = new JLabel("Team leader email:");
 		contentPane.add(lblTeamLeaderEmail, "2, 6");
-		
+
 		JLabel teamLeaderEmailDetails = new JLabel("");
 		contentPane.add(teamLeaderEmailDetails, "4, 6");
-		
+
 		JLabel lblTeamMembers = new JLabel("Team members:");
 		contentPane.add(lblTeamMembers, "2, 8");
-		
+
 		JLabel teamMembersDetails = new JLabel("");
 		contentPane.add(teamMembersDetails, "4, 8");
-		
+
 		JLabel lblLogo = new JLabel("Logo:");
 		contentPane.add(lblLogo, "2, 10");
-		
+
 		JLabel logo = new JLabel("");
 		contentPane.add(logo, "4, 10");
-		
+
 		JLabel lblVersion = new JLabel("Version:");
 		contentPane.add(lblVersion, "2, 12");
-		
+
 		JLabel versionDetails = new JLabel("");
 		contentPane.add(versionDetails, "4, 12");
 	}
