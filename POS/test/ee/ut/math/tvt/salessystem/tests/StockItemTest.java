25eaca7c1332f632f8d36199f4292e31a345e801
PepticSet <karl.klein5@gmail.com>
2014-11-20 12:30:21 +0200

    test related ANT targets, test class for testing testing :D

---
+++ POS/test/ee/ut/math/tvt/salessystem/tests/StockItemTest.java
@@ -0,0 +1,24 @@
+package ee.ut.math.tvt.salessystem.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StockItemTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Test
+	public void testGetColumn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+}
