22fe38911822b37d91f06583d31ae7a82a3a981f
Martin Valgur <martin.valgur@gmail.com>
2014-11-25 19:34:30 +0200

    Completed StockItemTest.java and SoldItemTest.java.

--- POS/test/ee/ut/math/tvt/salessystem/tests/SoldItemTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/tests/SoldItemTest.java
@@ -1,24 +1,52 @@
 package ee.ut.math.tvt.salessystem.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
 public class SoldItemTest {
+
+	private static StockItem stockItem1;
+	private static long id;
+	private static String name;
+	private static double price;
+	private static String description;
+	private static int stockItemQuantity;
+
+	private static SoldItem soldItem1;
+	private static int soldItem1Quantity;
+	private static SoldItem soldItem2;
+
+	private static double eps = 1e-3;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		id = 5;
+		name = "testitem";
+		price = 29;
+		description = "a test item";
+		stockItemQuantity = 7;
+		stockItem1 = new StockItem(id, name, description, price, stockItemQuantity);
+
+		soldItem1Quantity = 5;
+		soldItem1 = new SoldItem(stockItem1, soldItem1Quantity);
+
+		soldItem2 = new SoldItem(stockItem1, 0);
 	}
 
 	@Test
 	public void testGetSum() {
-		fail("Not yet implemented");
+		double sum = soldItem1.getSum();
+		assertEquals(soldItem1Quantity * price, sum, 1e-5);
 	}
-	
+
 	@Test
 	public void testGetSumWithZeroQuantity() {
-		fail("Not yet implemented");
+		assertEquals(0, soldItem2.getSum(), 1e-5);
 	}
 
 }
--- POS/test/ee/ut/math/tvt/salessystem/tests/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/tests/StockItemTest.java
@@ -1,24 +1,54 @@
 package ee.ut.math.tvt.salessystem.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class StockItemTest {
 
+	private static StockItem stockItem1;
+	private static long id;
+	private static String name;
+	private static double price;
+	private static String description;
+	private static int quantity;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		id = 5;
+		name = "testitem";
+		price = 29;
+		description = "a test item";
+		quantity = 7;
+		stockItem1 = new StockItem(id, name, description, price, quantity);
 	}
-	
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Test
 	public void testGetColumn() {
-		fail("Not yet implemented");
+		assertEquals(stockItem1.getColumn(0), id);
+		assertEquals(stockItem1.getColumn(1), name);
+		assertEquals(stockItem1.getColumn(2), price);
+		assertEquals(stockItem1.getColumn(3), quantity);
+		thrown.expect(RuntimeException.class);
+		stockItem1.getColumn(4);
 	}
 
 	@Test
 	public void testClone() {
-		fail("Not yet implemented");
+		StockItem clonedItem = (StockItem) stockItem1.clone();
+		assertEquals(stockItem1.getId(), clonedItem.getId());
+		assertEquals(stockItem1.getName(), clonedItem.getName());
+		assertEquals(stockItem1.getPrice(), clonedItem.getPrice(), 1e-5);
+		assertEquals(stockItem1.getDescription(), clonedItem.getDescription());
+		assertEquals(stockItem1.getQuantity(), clonedItem.getQuantity());
 	}
 
 }
