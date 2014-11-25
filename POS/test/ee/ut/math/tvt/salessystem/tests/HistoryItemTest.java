37193f7d68ffdcdcdeb4c2c9a976d7f8a0d69d02
Martin Valgur <martin.valgur@gmail.com>
2014-11-25 21:10:52 +0200

    Created a stub for SaleTest/PurchaseTest equivalent.

---
+++ POS/test/ee/ut/math/tvt/salessystem/tests/HistoryItemTest.java
@@ -0,0 +1,36 @@
+package ee.ut.math.tvt.salessystem.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This tests the equivalent of SaleTest/PurchaseTest in this POS.
+ */
+public class HistoryItemTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testAddSoldItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSumWithNoItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSumWithOneItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSumWithMultipleItems() {
+		fail("Not yet implemented");
+	}
+}
